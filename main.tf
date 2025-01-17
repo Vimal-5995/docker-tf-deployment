9194dcd638994f7faa39cf331a4c6e806dc34807
Vimal-5995 <vimalpt484@gmail.com>
2025-01-18 00:38:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,79 @@
-provider "docker" {}
+# Configure the Docker repository
+resource "null_resource" "docker_repo" {
+  provisioner "remote-exec" {
+    connection {
+      host        = "${var.vm_ip}"
+      user        = "${var.vm_user}"
+      private_key = file("${path.module}/id_rsa") 
+    }
+    inline = [
+      "sudo add-apt-repository \
+       'deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) stable'",
+    ]
+  }
+}
+
+# Update package lists
+resource "null_resource" "update_apt" {
+  triggers = toset([null_resource.docker_repo.triggers])
+  provisioner "remote-exec" {
+    connection {
+      host        = "${var.vm_ip}"
+      user        = "${var.vm_user}"
+      private_key = file("${path.module}/id_rsa") 
+    }
+    inline = [
+      "sudo apt-get update",
+    ]
+  }
+}
+
+# Install Docker Engine
+resource "null_resource" "install_docker" {
+  triggers = toset([null_resource.update_apt.triggers])
+  provisioner "remote-exec" {
+    connection {
+      host        = "${var.vm_ip}"
+      user        = "${var.vm_user}"
+      private_key = file("${path.module}/id_rsa") 
+    }
+    inline = [
+      "sudo apt-get install -y docker.io",
+    ]
+  }
+}
+
+# Start Docker service
+resource "null_resource" "start_docker" {
+  triggers = toset([null_resource.install_docker.triggers])
+  provisioner "remote-exec" {
+    connection {
+      host        = "${var.vm_ip}"
+      user        = "${var.vm_user}"
+      private_key = file("${path.module}/id_rsa") 
+    }
+    inline = [
+      "sudo systemctl start docker",
+    ]
+  }
+}
+
+# Verify Docker installation (optional)
+# ... (Similar to local installation, but using remote-exec)
+
+
+
+
+
+
+
+
+
+
+
+
+/** provider "docker" {}
 
 resource "docker_image" "nginx-image" {
   name = "nginx"
@@ -17,4 +92,4 @@ resource "docker_container" "nginx" {
 output "url" {
   description = "Browser URL for container site"
   value       = join(":", ["http://localhost", tostring(var.external_port)])
-}
+}**/
