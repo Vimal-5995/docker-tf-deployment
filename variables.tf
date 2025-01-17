a65d562dc897cd2b2e26918142a0ab0e2080968c
Vimal-5995 <vimalpt484@gmail.com>
2025-01-18 00:39:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,25 @@
-variable "external_port" {
+variable "vm_ip" {
+  type        = string
+  description = "IP address of the VirtualBox VM"
+}
+
+variable "vm_user" {
+  type        = string
+  description = "Username for SSH access to the VM"
+}
+
+
+
+
+
+
+
+
+/**variable "external_port" {
   type    = number
   default = 8080
   validation {
     condition     = can(regex("8080|80", var.external_port))
     error_message = "Port values can only be 8080 or 80."
   }
-}
+}**/
